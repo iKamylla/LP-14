db0c7fa6c6516674b9f3e20813a590d344e42563
Kamylla <>
2022-10-07 19:57:31 +0000

    Initial commit

---
+++ conversor.py
@@ -0,0 +1,2 @@
+def valor_pol(valor_cm):
+  return valor_cm / 2.54
---
+++ main.py
@@ -0,0 +1,9 @@
+from conversor import valor_pol
+
+valor_cm = int(input('Insira o valor em centímetros: '))
+
+file = open('Arquivo', 'w+')
+file.write(f'\nO valor {valor_cm} em centímetros corresponde a {valor_pol(valor_cm)} valor em polegadas.')
+file.seek(0,0)
+print(file.read())
+file.close()
